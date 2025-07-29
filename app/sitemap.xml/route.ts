a9f43ff1006653f7e4abc352ff9e59d47e750353
kingsgrimbyte <kingsgrimbyte@gmail.com>
2025-07-29 07:39:06 +0000

    new update today_data

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -16,7 +16,6 @@ export async function GET() {
     `${contentData.baseUrl}contact`,
     `${contentData.baseUrl}subdomains/sitemap.xml`,
     `${contentData.baseUrl}blogs/sitemap.xml`,
-    ...SubDomain.map((location: any) => `https://${location}.${contentData.host}`)
   ];
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
